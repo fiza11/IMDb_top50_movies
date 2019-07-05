fd9c414e845ccf70ab4bdbbdfa7504c3f77dc5e8
Fiza Husain <43874107+fiza11@users.noreply.github.com>
2019-07-05 21:18:10 +0530

    Update imdb_top50.py

--- imdb_top50.py
+++ imdb_top50.py
@@ -2,10 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 year1 = input("Enter 1st year")
 year2 = input("Enter 2nd year")
-if (year2 < year1):
-	a = year1
-	year1 = year2
-	year2 = a
 url = "https://www.imdb.com/search/title/?release_date=" + year1 + "," + year2 + "&title_type=feature"
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
