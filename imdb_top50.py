a38092d586edb9b3b5b0e2bbf9f158c59e7cef28
fizahusain1110 <fizahusain1110@gmail.com>
2019-07-05 21:03:08 +0530

    Initial Commit

---
+++ imdb_top50.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+year1 = input()
+year2 = input()
+if (year2 < year1):
+	a = year1
+	year1 = year2
+	year2 = a
+url = "https://www.imdb.com/search/title/?release_date=" + year1 + "," + year2 + "&title_type=feature"
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser')
+list = soup.findAll('h3', attrs={'class':'lister-item-header'})
+f = 1
+for i in list:
+	a_tag = i.find('a')
+	movie = a_tag.text
+	print(f, end = ". ")
+	print(movie)
+	f += 1
+
